665589fbdb874b45fbdbd45f1f928fd8f88d7b4e
Clair <yintai.com>
2016-02-19 10:06:39 +0800

    Initial Commit

---
+++ SpotlightDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotlightDemo
+//
+//  Created by yangyue on 16/2/19.
+//  Copyright © 2016年 com.yy.www. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
